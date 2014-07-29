9497c50ce5c7f33a6d89dcfdfe6980de7576b5aa
Chris Roeder <croeder@croeder.com>
2014-07-29 07:30:33 -0600

    checkpoint: working on creating a list of all tokens and their spans

--- src/clojure_craft/craft_pos.clj
+++ src/clojure_craft/craft_pos.clj
@@ -6,30 +6,43 @@
 (def base "/home/croeder/git/craft/craft-1.0/genia-xml/pos")
 (def sample-article "11532192.txt.xml")
 (def text-file (str base "/" sample-article))
-;{:tag :sentence, :attrs nil, :content [{:tag :tok, :attrs {:cat "NN"}, :content ["Abstract"]}]} 
+
+;; FSCK! we need a list of all the tokens and their  spans to use as keys into
+;; each of the onotology data and dep. parse.
+;; - use the plain text and this file (genia pos) to reconstruct.
+
+(defrecord Token [file start end pos text] )
+
 (defn read-craft-file [file]
 	(xml-seq (parse (java.io.File. file))))
 
-(defn parse-sentence [sentence]
-  (loop [tokens (rest sentence)
+(defn find-spans [])
+
+;---> {:tag :tok, :attrs {:cat JJ}, :content [specific]}
+(defn parse-tokens [sentence]
+  (loop [tokens sentence
          token (first sentence)
          spans []]
+    (println "--->" token)
     (cond (not (empty? tokens))
           (recur 
            (rest tokens) 
            (first tokens) 
-           (conj spans (list 'start 'end 'pos 'span))) ;; yeah, gonna need some more work here, okay?
-          :t 
-          (conj spans (list 'start 'end 'pos  'span)))
-  "x")
+           (conj spans (Token. 'file 
+                              'start 'end 
+                              (:cat (:attrs token)) 
+                              (first (:content token)))))
+          :t  
+          spans)))
 
-(defn load-tokens-from-xml [file]
+;; ... {  ... :content {:tag :sentence, :attrs nil, :content [{:tag :tok, :attrs {:cat "NN"}, :content ["Abstract"]}]} }
+(defn load-from-xml [file]
 	(let [in-data (:content (first (read-craft-file text-file)))]
-               (loop [item (first in-data)
+               (loop [sentence (first in-data)
                      data (rest in-data)
                       sentence-list []]
                  (cond (not (empty? data)) 
-                       (recur (first data) (rest data) (conj sentence-list (parse-sentence item)))
+                       (recur (first data) (rest data) (conj sentence-list (parse-tokens (:content sentence))))
                        :t
                        nil))))
  
