7da41a806cdf90cd117a172ec3c4b0d28763b5f1
Chris Roeder <croeder@croeder.com>
2014-08-20 12:18:13 -0600

    dependency in good shape as described in README

--- src/clojure_craft/craft_dep.clj
+++ src/clojure_craft/craft_dep.clj
@@ -1,9 +1,12 @@
 (ns clojure-craft.craft-dep)
+(use 'clojure.java.io)
 (use '[clojure.string :only (join split)])
-
+;;;; TODO: need to find start/end for the literals
 
 (def base-data-dir "/home/croeder/git/craft/craft-1.0/dependency")
-(def file "11532192.dep")
+(def dep-file "11532192.dep")
+(def dep-file "short.dep")
+(def sample-file (str base-data-dir "/" dep-file))
 
 (def sample-sentence (list
 "1 Intraocular intraocular JJ  _ 2 NMOD"
@@ -48,38 +51,58 @@
 ;; dep-code: ROOT NMOD PMOD DEP COORD CONJ
 ;; pos: JJ NN IN RB JJ NNS DT CC
 
-(defstruct dep  :id :literal :normalized :pos :dep-id :dep-type :start :end)
-(defn parse-dependency-file
-""
-[file]
-
-)
-
-(defn parse-dependency-sentence
-"parses a paragraph of a dep file representing a sentence,
-returns a map keyed by id of the items. 
-key: \"11\" 
-value: {:id \"11\", :literal \"strain\", :normalized \"strain\", :pos \"NN\", :dep-id \"12\", :dep-type \"NMOD\"}
-"
-[item-list]
-  (loop [my-list (rest item-list)
-         item (first item-list)
-         parts-map {} ]
-    (cond (not (empty? my-list))
-          (let [parsed-item (parse-dependency-item item)]
-            (recur (rest my-list) (first my-list) 
-                   (assoc parts-map (:id parsed-item) parsed-item)))
-      :t parts-map)))
-
+;                     -1             0          1         2          3    5       6
+(defstruct Dependency :sentence-num  :token-num :literal :normalized :pos :dep-id :dep-type )
 
 (defn parse-dependency-item
 "parses a single line of the dependency file"
-[item-string]
+[item-string sentence-num]
   (let [parts (split item-string #"\s+")]
-    (struct dep (nth parts 0)
+    (struct Dependency 
+            sentence-num
+            (nth parts 0)
             (nth parts 1)
             (nth parts 2)
             (nth parts 3)
             (nth parts 5)
             (nth parts 6)))
 )
+
+
+(defn create-sos-from-paragraphs
+"given a file of lines grouped into paragraphs with blank lines,
+create a sequence-of-sequences representing a sequence of paragraphs
+each holding sentences as sequences of tokens. There is potential for
+confusion because the jenia file is one line per token of the source
+text. Lines in jenia file represent tokens in the source. Paragraphs
+in the jenia file represent lines in the source. Paragraphs in the source
+are not distinguished."
+[dep-file]
+(with-open [buffered-reader (java.io.BufferedReader. (java.io.FileReader. dep-file))]
+	         (loop [line (.readLine buffered-reader)
+                        sos [] 
+                        paragraph []]
+                   (cond 
+                    (not line)  (conj sos paragraph)
+                    (re-matches #"^\d+.*"  line) (recur (.readLine buffered-reader) 
+                                                         sos (conj paragraph line))
+                    :t  (recur (.readLine buffered-reader) 
+                               (conj sos paragraph) []) ))))
+
+(defn parse-line
+" parse a jenia paragraph that represents a source line/sentence"
+[jenia-paragraph sentence-number]
+(map (fn [x] 
+       (parse-dependency-item x sentence-number))
+     jenia-paragraph))
+
+
+(defn parse-sos 
+"parse a sequence of jenia paragraphs, numbering them"
+[sos]
+(map (fn [x sentence-number]
+       (parse-line x sentence-number))
+     sos (iterate inc 1)))
+
+(defn glue [file]
+  (parse-sos (create-sos-from-paragraphs file)))
